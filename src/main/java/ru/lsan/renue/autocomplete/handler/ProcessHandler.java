51977b89b4e282e86148ba41de291146daf2a800
L-san <lsarelli9402@gmail.com>
2023-02-21 16:55:28 +0400

    made path constant
    easier to change for tests

--- src/main/java/ru/lsan/renue/autocomplete/handler/ProcessHandler.java
+++ src/main/java/ru/lsan/renue/autocomplete/handler/ProcessHandler.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 public class ProcessHandler {
 
+    private static final String PATH = "C:/airports.csv";
+
     public static void handle(String pattern, int nCol) {
         long start = System.currentTimeMillis();
-        List<String> list = CSVSearch.search("C:/airports.csv", nCol, pattern);
+        List<String> list = CSVSearch.search(PATH, nCol, pattern);
         long stop = System.currentTimeMillis();
         for (String s : list) {
             System.out.println(s);
